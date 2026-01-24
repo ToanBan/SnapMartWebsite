bfdda33b62f594b5c849496724c90e990ff43fdb
letoanban <letoanban.word@gmail.com>
2026-01-24 20:53:56 +0700

    change variable env

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ const {
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: `${process.env.FRONTEND_URL || "http://localhost:3000"}`,
     methods: ["GET", "POST"],
     credentials: true,
   },
